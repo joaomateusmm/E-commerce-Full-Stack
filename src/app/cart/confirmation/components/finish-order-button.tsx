845d3cd883ebf480a16e8489f2e9752ae4d6a4ed
Joao Mateus <joaomateusmb@gmail.com>
2025-08-11 22:34:05 -0300

    chore: fix the authenyificatin and try checkout

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -2,6 +2,7 @@
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
 
 import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
@@ -10,33 +11,47 @@ import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 const FinishOrderButton = () => {
   const finishOrderMutation = useFinishOrder();
   const handleFinishOrder = async () => {
-    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Stripe publishable key is not set");
+    try {
+      if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+        throw new Error("A chave publicável do Stripe não está configurada.");
+      }
+      const { orderId } = await finishOrderMutation.mutateAsync();
+      const checkoutSession = await createCheckoutSession({
+        orderId,
+      });
+
+      if (!checkoutSession || !checkoutSession.id) {
+        throw new Error("Não foi possível criar a sessão de checkout.");
+      }
+
+      const stripe = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+      );
+      if (!stripe) {
+        throw new Error("Não foi possível carregar o Stripe.");
+      }
+      await stripe.redirectToCheckout({
+        sessionId: checkoutSession.id,
+      });
+    } catch (error) {
+      console.error("Finish Order Error:", error);
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("Ocorreu um erro ao finalizar a compra.");
+      }
     }
-    const { orderId } = await finishOrderMutation.mutateAsync();
-    const checkoutSession = await createCheckoutSession({
-      orderId,
-    });
-    const stripe = await loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    );
-    if (!stripe) {
-      throw new Error("Failed to load Stripe");
-    }
-    await stripe.redirectToCheckout({
-      sessionId: checkoutSession.id,
-    });
   };
   return (
     <>
       <Button
-        className="w-full rounded-full py-6 hover:scale-[1.02] cursor-pointer"
+        className="w-full cursor-pointer rounded-full py-6 hover:scale-[1.02]"
         size="lg"
         onClick={handleFinishOrder}
         disabled={finishOrderMutation.isPending}
       >
         {finishOrderMutation.isPending && (
-          <Loader2 className="h-4 w-4 animate-spin" />
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
         )}
         Finalizar compra
       </Button>
